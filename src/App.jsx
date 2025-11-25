967827474f08711d914af8aa75f0c2df030fb76d
donyamardani <zmardani0@gmail.com>
2025-11-26 02:57:05 +0330

    create pages and components and Utils file and app
    create pages and components and Utils file and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes,Route,Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import AuthContext from './Utils/AuthContext'
 
 export default function App() {
+  const [token,setToken]=useState(null);
+  const handleToken=(tk)=>{
+    setToken(tk)
+  }
   return (
     <>
+    <AuthContext.Provider value={{handleToken,token}}>
+
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+      
     <Toaster/>
+    </AuthContext.Provider>
     </>
 
   )
