3a91b7e5acf34b2a154fb706593c272a90de9944
donyamardani <zmardani0@gmail.com>
2025-11-25 20:24:07 +0330

    create basic file
    create basic file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes,Route,Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+export default function App() {
+  return (
+    <>
+    <Toaster/>
+    </>
+
+  )
+}
