9dbb162dd28a7c923c3af1c72c232c225f949f8b
donyamardani <zmardani0@gmail.com>
2025-11-26 04:38:55 +0330

    add slider,and not found page
    add slider,and not found page

--- src/Pages/Home/Slider/index.jsx
+++ src/Pages/Home/Slider/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './style.css';
+import { Pagination, Navigation } from 'swiper/modules';
+
 
 export default function Slider() {
   return (
-    <div>Slider</div>
-  )
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {items}
+      </Swiper>
+    </>
+  );
+    
+  
 }
--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <section style={{ textAlign: "center", padding: "4rem 1rem" }}>
+      <h1>404</h1>
+      <p>صفحه‌ای که دنبال آن بودید پیدا نشد.</p>
+      <Link to="/" className="btn-outline">بازگشت به خانه</Link>
+    </section>
+  );
 }
