10b951af61959153915fff88e862498b5e5a69d8
donyamardani <174435998+donyamardani@users.noreply.github.com>
2025-11-27 02:49:11 +0330

    create slider to home page
    create slider to home page

--- src/Pages/Home/Slider/index.jsx
+++ src/Pages/Home/Slider/index.jsx
@@ -1,16 +1,47 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import './style.css';
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
-
 export default function Slider() {
-  const [slide,setSlide]=useState()
+    const [slide,setSlide]=useState();
+  useEffect(() => {
+    (async () => {
+        const url =
+          "https://sephora.p.rapidapi.com/products/v2/list?number=1&size=4&country=SG&language=en-SG&sort=sales";
+        const options = {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key":
+              "793e61b096msh0180bc0bcfc492bp1a3463jsna5f724afc1c4",
+            "x-rapidapi-host": "sephora.p.rapidapi.com",
+          }
+        };
   
+        try {
+          const res = await fetch(url, options);
+          const data = await res.json(); 
+          let images=[]; 
+           data.data?.map(
+                    e=>{images.push(e.attributes?.["image-urls"][0])
+                        images.push(e.attributes?.["image-urls"][1])
+                   });
+        setSlide(images);
+        } catch (error) {
+          console.error(error);
+        }
+      })()
+  },[]);
+  const items=slide?.map((e,index)=><SwiperSlide key={index}><img src={e} alt="" /></SwiperSlide>)
   return (
     <>
       <Swiper
@@ -22,12 +53,10 @@ export default function Slider() {
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
-        className="mySwiper"
+        className={"mySwiper"}
       >
-        {items}
+       {items}
       </Swiper>
     </>
   );
-    
-  
 }
