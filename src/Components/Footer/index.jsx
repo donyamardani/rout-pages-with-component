979620020e52e3b4afcaa066316048ac9dd21109
donyamardani <174435998+donyamardani@users.noreply.github.com>
2025-11-30 00:51:15 +0330

    create footer
    create footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import 'boxicons'
+import style from './style.module.css'
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={style.Footer}>
+      <div className={style.text}>
+        <ul>
+          <li><a href="#">abute us</a></li>
+          <li><a href="#">abute brands</a></li>
+          <li><a href="#">abute logo</a></li>
+          <li><a href="#">abute company</a></li>
+        </ul>
+      </div>
+      <div className={style.media}>
+        <ul>
+          <li ><a href="#"><box-icon type='logo' name='instagram' size='md' color='white'/></a></li>
+          <li ><a href="#"><box-icon type='logo' name='twitter' size='md'color='white'/></a></li>
+          <li ><a href="#"><box-icon type='logo' name='facebook' size='md'color='white'/></a></li>
+          <li><a href="#"><box-icon type='logo' name='telegram' size='md'color='white'/></a></li>
+        </ul>
+      </div>
+    </div>
   )
 }
